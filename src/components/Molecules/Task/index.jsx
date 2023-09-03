23849c7fbf82184eca3cb100de0dbddb90602a5b
MoriguchiHinata <h.moriguchi.23.5.966@4nonome.com>
2023-09-03 14:50:08 +0900

    横幅、typoの修正

--- src/components/Molecules/Task/index.jsx
+++ src/components/Molecules/Task/index.jsx
@@ -1,14 +1,14 @@
-import {React, useState} from "react";
-import Checkbox from "../../Atoms/Checkbox/index.jsx"
-import Input from "../../Atoms/Input/index.jsx"
-import EditButton from "../../Atoms/EditButton/index.jsx"
+import { React, useState } from "react";
+import Checkbox from "../../Atoms/Checkbox/index.jsx";
+import Input from "../../Atoms/Input/index.jsx";
+import EditButton from "../../Atoms/EditButton/index.jsx";
 import styled from "styled-components";
 import BREAKPOINT from "../../../variables/breakpoint";
-import COLOR from "../../../variables/color"
-import FONT_FAMILY from "../../../variables/font_family"
-import TEXTS from "../../../variables/texts"
+import COLOR from "../../../variables/color";
+import FONT_FAMILY from "../../../variables/font_family";
+import TEXTS from "../../../variables/texts";
 
-const Task = ({onTaskChange, onTaskComplete, taskName, defaultIsEditing}) => {
+const Task = ({ onTaskChange, onTaskComplete, taskName, defaultIsEditing }) => {
   const [isEditing, setEditing] = useState(defaultIsEditing);
 
   const onEditComplete = (value) => {
@@ -22,20 +22,22 @@ const Task = ({onTaskChange, onTaskComplete, taskName, defaultIsEditing}) => {
 
   return (
     <StyledWrapper>
-      <SytledCheckboxWrapper><Checkbox onClick={onTaskComplete}/></SytledCheckboxWrapper>
-      {isEditing
-        ? <Input onEditComplete={onEditComplete} defaultValue={taskName}/>
-        : (
-            <StyledNameAndButtonWrapper>
-              <StyledTaskName>{taskName}</StyledTaskName>
-              <StyledEditButtonWrapper><EditButton onClick={onEditButtonClick}/></StyledEditButtonWrapper>
-            </StyledNameAndButtonWrapper>
-          )
-      }
+      <SytledCheckboxWrapper>
+        <Checkbox onClick={onTaskComplete} />
+      </SytledCheckboxWrapper>
+      {isEditing ? (
+        <Input onEditComplete={onEditComplete} defaultValue={taskName} />
+      ) : (
+        <StyledNameAndButtonWrapper>
+          <StyledTaskName>{taskName}</StyledTaskName>
+          <StyledEditButtonWrapper>
+            <EditButton onClick={onEditButtonClick} />
+          </StyledEditButtonWrapper>
+        </StyledNameAndButtonWrapper>
+      )}
     </StyledWrapper>
   );
 };
-
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -47,17 +49,16 @@ const StyledWrapper = styled.div`
 
 const SytledCheckboxWrapper = styled.div`
   display: flex;
-  justiyfy-content: center;
+  justify-content: center;
   align-items: center;
 `;
 
 const StyledNameAndButtonWrapper = styled.div`
   display: flex;
   flex-direction: row;
-  justiyfy-content: center;
+  justify-content: center;
   align-items: center;
   gap: 10px;
-  max-width: 214px;
   width: 100%;
 `;
 
