c582d12c769b8fb29f6b5a1cc93e653a0a624585
MoriguchiHinata <h.moriguchi.23.5.966@4nonome.com>
2023-09-27 01:16:02 +0900

    Alertの作成

---
+++ src/components/Atoms/Alert/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import BREAKPOINT from "../../../variables/breakpoint";
+import COLOR from "../../../variables/color";
+import FONT_FAMILY from "../../../variables/font_family";
+import TEXTS from "../../../variables/texts";
+
+const Alert = ({ isActive, alertText }) => {
+  console.log(isActive);
+  console.log(alertText);
+  return <StyledAlert isActive={isActive}>{alertText}</StyledAlert>;
+};
+
+const StyledAlert = styled.div`
+  opacity: ${({ isActive }) => (isActive ? `1` : `0`)};
+  top: ${({ isActive }) => (isActive ? `80px` : `0px`)};
+  position: absolute;
+  /* left: 50%;
+  transform: translate(-50%, 0); */
+  transition: 0.5s ease-out;
+  font-family: ${FONT_FAMILY.NOTO_SANS};
+  color: ${COLOR.WHITE};
+  ${TEXTS.S}
+  background-color: ${COLOR.RED};
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  border-radius: 4px;
+  padding: 10px 20px;
+  width: 100%;
+  max-width: 400px;
+
+  @media (max-width: ${BREAKPOINT.MEDIUM}) {
+    top: ${({ isActive }) => (isActive ? `40px` : `0px`)};
+    margin: 0 20px;
+    width: calc(100% - 40px);
+  }
+`;
+
+export default Alert;
---
+++ src/components/contexts/alert_handler.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from "react";
+
+const AlertHandlerContext = createContext();
+
+export const AlertHandlerProvider = ({ children }) => {
+  const [alertState, setAlertState] = useState({
+    visible: false,
+    errorText: "",
+  });
+
+  const setAlert = (errorText) => {
+    const alertStateCpy = Object.assign({}, alertState);
+    alertStateCpy.visible = true;
+    alertStateCpy.errorText = errorText;
+    setAlertState(alertStateCpy);
+  };
+
+  const closeAlert = () => {
+    const alertStateCpy = Object.assign({}, alertState);
+    alertStateCpy.visible = false;
+    setAlertState(alertStateCpy);
+  };
+
+  const contextValue = {
+    ...alertState,
+    setAlert: setAlert,
+    closeAlert: closeAlert,
+  };
+
+  return (
+    <AlertHandlerContext.Provider value={contextValue}>
+      {children}
+    </AlertHandlerContext.Provider>
+  );
+};
+
+export const useAlertHandlerContext = () => useContext(AlertHandlerContext);
